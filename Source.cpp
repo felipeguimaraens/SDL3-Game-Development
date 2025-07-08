47a04c99a3a57e59bd29f5e1effa699403d09dbb
felipeguimaraens <felipeguimaraens@hotmail.com>
2025-07-08 02:28:25 -0300

    Created init and render custom functions

--- Source.cpp
+++ Source.cpp
@@ -8,31 +8,51 @@ SDL_Renderer* gameRenderer = 0;
 
 SDL_InitFlags gameInitFlags = SDL_INIT_VIDEO;
 
+bool isRunning = false;
+
+bool init(const char* title, int height, int width, int window_flags) {
+	if (!SDL_Init(gameInitFlags)) {
+		throw std::runtime_error("Unable to create window");
+	}
+
+	gameWindow = SDL_CreateWindow(title, height, width, window_flags); // 0 = No flags
+
+	if (!gameWindow) {
+		throw std::runtime_error("Unable to create window");
+	}
+
+	gameRenderer = SDL_CreateRenderer(gameWindow, NULL);
+
+	if (!gameRenderer) {
+		throw std::runtime_error("Unable to create renderer");
+	}
+
+	isRunning = true;
+
+	return true;
+}
+
+void render() {
+	SDL_SetRenderDrawColor(gameRenderer, 0, 0, 255, 255);
+	SDL_RenderClear(gameRenderer);
+	SDL_RenderPresent(gameRenderer);
+}
+
 int main() {
 	try {
 
-		if (!SDL_Init(gameInitFlags)) {
-			throw std::runtime_error("Unable to init SDL");
+		init("SDL: Broken in functions", 800, 600, 0);
+
+		while (isRunning) {
+			render();
 		}
 		
-		gameWindow = SDL_CreateWindow("SDL: BSOL - Blue Screen of Life", 800, 600, 0); // 0 = No flags
-		if (!gameWindow) {
-			throw std::runtime_error("Unable to create window");
-		}
-
-		gameRenderer = SDL_CreateRenderer(gameWindow, NULL);
-		if (!gameRenderer) {
-			throw std::runtime_error("Unable to create renderer");
-		}
-
-		SDL_SetRenderDrawColor(gameRenderer, 0,0,255,255);
-		SDL_RenderClear(gameRenderer);
-		SDL_RenderPresent(gameRenderer);
+		
 
 		// To finish the program
 		SDL_Delay(5000);
 		SDL_Quit();
-
+		
 	}
 	catch (std::runtime_error& e) {
 		std::cout << e.what() << "\n" << "SDL error: " << SDL_GetError();
